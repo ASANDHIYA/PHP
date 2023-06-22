bb226a8065c665deb282806f83d2d7686468b3e8
ASANDHIYA <88929308+ASANDHIYA@users.noreply.github.com>
2023-06-22 14:20:31 +0530

    Add files via upload

---
+++ CRUD/form.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            Crud Operations
+</title>
+</head> 
+<body>
+    <h2> Student Form</h2>
+     <Form>
+        Name: <input type="text" name="name">
+        Age : <input type="number" name="age"><br><br>
+        Gender:<select name="gender">
+            <option value="">Select gender</option>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+            </select><br><br>
+            Email id:<input type="email" name="emailid"><br><br>
+            Phone number:<input type="tel" name="phonenumber"><br><br>
+            Address:<textarea rows=4 cols=50></textarea><br><br>
+
+
+
+     </Form>
+</body>   
+         
+</html>
+
